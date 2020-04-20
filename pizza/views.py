45e9c0d187a2a2dfc7ad820b1400201156031fd1
Varun Garg <v8garg@uwaterloo.ca>
2020-04-20 12:26:01 -0400

    Add option to save a pizza order to the database

--- pizza/views.py
+++ pizza/views.py
@@ -12,6 +12,7 @@ def order(request):
     if request.method == 'POST':
         filled_form = PizzaForm(request.POST)
         if filled_form.is_valid():
+            filled_form.save()
             note = 'Thanks for ordering! Your %s %s and %s pizza is on its way!' %(filled_form.cleaned_data['size'],
             filled_form.cleaned_data['topping1'],
             filled_form.cleaned_data['topping2'],)
